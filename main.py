883f63e2cffb11e476531c8d8f3305e3e818a1a2
Lucas Amiune <lucastintin@gmail.com>
2025-04-24 18:09:11 -0300

    Update main.py
    Price Range

--- main.py
+++ main.py
@@ -4,17 +4,38 @@ import requests
 import xml.etree.ElementTree as ET
 import time
 import json as JSON
+from collections import Counter
 
-def fetch_game_mechanics(paramGameId):
+def contarMecanicasCategorias(jogos_ids):
+    todas_mecanicas = []
+    todas_categorias = []
+
+    for game_id in jogos_ids:
+        info = fetch_game_mechanics_and_categories(game_id["id"])
+        todas_mecanicas.extend(info["mechanics"])
+        todas_categorias.extend(info["categories"])
+        time.sleep(3)  # evitar overload na API
+
+    contagem_mec = Counter(todas_mecanicas)
+    contagem_cat = Counter(todas_categorias)
+
+    return contagem_mec.most_common(), contagem_cat.most_common()
+
+def fetch_game_mechanics_and_categories(paramGameId):
     url = f"https://boardgamegeek.com/xmlapi2/thing?id={paramGameId}&type=boardgame&stats=0"
     response = requests.get(url)
     
     if response.status_code == 200:
         root = ET.fromstring(response.content)
-        mechanics = []
-        for link in root.findall(".//link[@type='mechanic']"):
-            mechanics.append(link.attrib["value"])
-        return mechanics
+        metadadosJogo = []
+        print(root)
+        for link in root.findall(".//link"):
+            metadadosJogo.append('a')
+            if link.attrib["type"] == "boardgamecategory" and link.attrib["value"] =='baordgamemechanics':
+                metadadosJogo.append('b')
+                metadadosJogo.append(link.attrib["value"])
+        print("Mecanicas: ", metadadosJogo)
+        return metadadosJogo
     else:
         return []
 
@@ -39,6 +60,7 @@ def fetch_price_USD(paramGameId):
                 prices.append({"price": float(price), "date": date})
     return prices
 
+##DEPRECATED
 def fetch_lastPrice_USD(paramGameId):
     uri = f"https://boardgamegeek.com/api/market/products/pricehistory?ajax=1&condition=any&currency=USD&objectid={paramGameId}&objecttype=thing&pageid=1"
     response = requests.get(uri)
@@ -77,7 +99,7 @@ def fetch_collection(username):
             name = item.find("name").text
             year_elem = item.find("yearpublished")
             year = year_elem.text if year_elem is not None else "?"
-            price = fetch_lastPrice_USD(game_id)
+            price = fetch_price_USD(game_id)[0]['price']
             games.append({"id": game_id, "name": name, "year": year, "price": price})
     return games
 
@@ -109,6 +131,7 @@ if st.button("Buscar coleção") and username:
                 minPriceTotal += float(minPrice)
 
                 priceTotal += float(game['price'])
+                #Montando o dataFrame
                 data.append({"name": game["name"], "last_sell": game['price'], 'min_price': minPrice, 'max_price': maxPrice})
             st.success(f"Coleção estimada entre (em USD$): {minPriceTotal:.2f} ~ {maxPriceTotal:.2f}")       
             
@@ -119,5 +142,15 @@ if st.button("Buscar coleção") and username:
                 st.write("A coleção foi estimada com base no preço da última venda realizada no BGG Market, independente da condição do jogo.")
                 st.dataframe(df, use_container_width=True, hide_index=True)
             st.toast("No detalhamento da coleção, há opção de Exportar para CSV. Pode ser importado no Excel, para você usar mais funções.", icon="🔔")
+
+            #Futuramente Apresentar um gráfico com as mecanicas
+            #mec_top, cat_top = contarMecanicasCategorias(collection)
+            #st.subheader("🧩 Mecânicas mais frequentes")
+            #for mec, count in mec_top[:10]:
+            #    st.write(f"{mec}: {count} jogos")
+
+            #st.subheader("🏷️ Categorias mais frequentes")
+            #for cat, count in cat_top[:10]:
+            #    st.write(f"{cat}: {count} jogos")
         else:
             st.warning("Nenhum jogo encontrado ou usuário inválido.")
